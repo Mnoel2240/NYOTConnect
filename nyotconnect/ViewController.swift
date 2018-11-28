18fb1939c1ed630ec653a221ca66f2971163209e
Khalil Tavarez <khaliltavarez@gmail.com>
2018-11-28 17:17:56 -0500

    Start Project

---
+++ nyotconnect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nyotconnect
+//
+//  Created by Code Next Student on 11/28/18.
+//  Copyright © 2018 nyotconnectteam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
